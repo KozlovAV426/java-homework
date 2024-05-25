ee0bc469ea16e26300ec15ffee7ffb7a218cfd77
surkinasasha <139327592+surkinasasha@users.noreply.github.com>
2024-05-26 01:22:33 +0500

    Update Size.java

--- src/main/java/edu/phystech/hw5/annotation/validation/Size.java
+++ src/main/java/edu/phystech/hw5/annotation/validation/Size.java
@@ -1,8 +1,9 @@
 package edu.phystech.hw5.annotation.validation;
 
-/**
- * @author kzlv4natoly
- */
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+@Retention(RetentionPolicy.RUNTIME)
 public @interface Size {
     int min() default 1;
     int max();
