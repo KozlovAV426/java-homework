cca09bb24e33c3c0d90a80e8d27c8d8b5efe1d27
surkinasasha <139327592+surkinasasha@users.noreply.github.com>
2024-05-26 01:13:35 +0500

    Update CacheUtils.java

--- src/main/java/edu/phystech/hw5/service/CacheUtils.java
+++ src/main/java/edu/phystech/hw5/service/CacheUtils.java
@@ -2,15 +2,14 @@ package edu.phystech.hw5.service;
 
 import java.lang.reflect.Proxy;
 
-/**
- * @author kzlv4natoly
- */
+
+@SuppressWarnings("unchecked")
 public class CacheUtils {
     public static <T> T getCacheProxy(Class<T> clazz, T object) {
-        // Здесь привиден пример того, как можно создавать динамический прокси
         return (T) Proxy.newProxyInstance(
                 CacheUtils.class.getClassLoader(),
                 new Class[]{clazz},
                 new CacheableInvocationHandler(object));
     }
 }
+
