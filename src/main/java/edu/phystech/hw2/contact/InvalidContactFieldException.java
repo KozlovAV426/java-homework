cde41541f82229d2ee1ce1b3bde65a0be404fb52
surkinasasha <139327592+surkinasasha@users.noreply.github.com>
2024-05-25 07:22:44 +0500

    Update InvalidContactFieldException.java

--- src/main/java/edu/phystech/hw2/contact/InvalidContactFieldException.java
+++ src/main/java/edu/phystech/hw2/contact/InvalidContactFieldException.java
@@ -1,12 +1,15 @@
 package edu.phystech.hw2.contact;
 
 public class InvalidContactFieldException extends RuntimeException {
-    private String fieldName;
 
-    public String getFieldName() {
-        return null;
+  
+        private String fieldName;
+    
+        public String getFieldName() {
+            return fieldName;
+        }
+    
+        public InvalidContactFieldException(String fieldName) {
+            this.fieldName = fieldName;
+        }
     }
-
-    InvalidContactFieldException(String fieldName) {
-    }
-}
