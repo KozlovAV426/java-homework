baafd0449ae1d8cabe7a5265386e570c2586e35b
surkinasasha <139327592+surkinasasha@users.noreply.github.com>
2024-05-26 01:21:02 +0500

    Update ValidationException.java

--- src/main/java/edu/phystech/hw5/exception/ValidationException.java
+++ src/main/java/edu/phystech/hw5/exception/ValidationException.java
@@ -1,7 +1,10 @@
 package edu.phystech.hw5.exception;
 
-/**
- * @author kzlv4natoly
- */
 public class ValidationException extends RuntimeException {
+    public ValidationException() {
+        super();
+    }
+    public ValidationException(String message) {
+        super(message);
+    }
 }
