625cac9d544d7bc8a7257aad6376b28a5e3fc8ec
surkinasasha <139327592+surkinasasha@users.noreply.github.com>
2024-05-25 08:22:53 +0500

    Update NegativeTextAnalyzer.java

--- src/main/java/edu/phystech/hw2/analyzer/NegativeTextAnalyzer.java
+++ src/main/java/edu/phystech/hw2/analyzer/NegativeTextAnalyzer.java
@@ -7,6 +7,17 @@ public class NegativeTextAnalyzer extends KeywordAnalyzer {
     private static final List<String> NEGATIVE_SMILES = List.of(":(", "=(", ":|");
 
     public NegativeTextAnalyzer() {
-        super(null, null);
+        super(NEGATIVE_SMILES, Label.NEGATIVE);
     }
+     @Override
+    public Label processText(String text) {
+        for (String keyword : NEGATIVE_SMILES) {
+            if (text.contains(" " + keyword + " ") || text.startsWith(keyword + " ") ||  text.endsWith(" " + keyword)) { 
+                return Label.NEGATIVE;
+            }
+        }
+        return Label.OK;
+    }
+    
 }
+
