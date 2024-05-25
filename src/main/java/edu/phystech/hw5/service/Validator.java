85ebe6edbb384a3eff00bf1be67fcf9e3a04c557
surkinasasha <139327592+surkinasasha@users.noreply.github.com>
2024-05-26 01:49:42 +0500

    Update Validator.java

--- src/main/java/edu/phystech/hw5/service/Validator.java
+++ src/main/java/edu/phystech/hw5/service/Validator.java
@@ -1,8 +1,11 @@
 package edu.phystech.hw5.service;
+import java.lang.reflect.Field;
 
 /**
  * @author kzlv4natoly
  */
+
 public interface Validator {
-    void validate(Object object);
+    void validate(Object object) throws IllegalAccessException;
+    void validateNotBlank(Field inputField, Object object) throws IllegalAccessException;
 }
