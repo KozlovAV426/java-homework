965a19bfbaa28d1332869deb14cc770570f399cd
surkinasasha <139327592+surkinasasha@users.noreply.github.com>
2024-05-26 01:12:54 +0500

    Update CacheableInvocationHandler.java

--- src/main/java/edu/phystech/hw5/service/CacheableInvocationHandler.java
+++ src/main/java/edu/phystech/hw5/service/CacheableInvocationHandler.java
@@ -1,26 +1,34 @@
 package edu.phystech.hw5.service;
 
-import java.lang.reflect.AccessibleObject;
+import edu.phystech.hw5.annotation.Cacheable;
 import java.lang.reflect.InvocationHandler;
-import java.lang.reflect.InvocationTargetException;
+import java.util.HashMap;
+import java.util.Map;
 import java.lang.reflect.Method;
-import java.util.Arrays;
 
-/**
- * @author kzlv4natoly
- */
+
+
 public class CacheableInvocationHandler implements InvocationHandler {
-    // Здесь необходимо реализовать логику по обработке @cacheable и вызову конкретного метода объекта
-
     private final Object target;
-
+    private final Map<Method, Map<Object, Object>> cache = new HashMap<>();
     public CacheableInvocationHandler(Object target) {
         this.target = target;
     }
 
     @Override
-    public Object invoke(Object o, Method method, Object[] arguments) throws Throwable
-    {
-       return method.invoke(target, arguments);
+    public Object invoke(Object o, Method method, Object[] arguments) throws Throwable {
+        if (method.isAnnotationPresent(Cacheable.class)) {
+            Map<Object, Object> methodCache = cache.computeIfAbsent(method, k -> new HashMap<>());
+            Object arg = arguments[0];
+            if (methodCache.containsKey(arg)) {
+                return methodCache.get(arg);
+            } else {
+                Object result = method.invoke(target, arguments);
+                methodCache.put(arg, result);
+                return result;
+            }
+        } else {
+            return method.invoke(target, arguments);
+        }
     }
 }
