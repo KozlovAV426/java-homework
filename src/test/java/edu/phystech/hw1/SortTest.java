19c11d691f4801e3e1031302fb5663c8f89e67b7
victormorozov1 <vismo@bk.ru>
2024-06-18 18:00:46 +0300

    Task 5

--- src/test/java/edu/phystech/hw1/SortTest.java
+++ src/test/java/edu/phystech/hw1/SortTest.java
@@ -6,9 +6,34 @@ import org.junit.Test;
 import org.junit.jupiter.api.Assertions;
 
 public class SortTest {
+    private static int[] sort(int[] nums, int l, int r) {
+        if (r - l == 1) {
+            return new int[] { nums[l] };
+        }
+        var s1 = sort(nums, l, (l + r) / 2);
+        var s2 = sort(nums,(l + r) / 2, r);
+
+        int[] result = new int[r - l];
+        int s1Index = 0;
+        int s2Index = 0;
+
+        while (s1Index + s2Index < r - l) {
+            if (s2Index == s2.length) {
+                result[s1Index + s2Index] = s1[s1Index++];
+            } else if (s1Index == s1.length) {
+                result[s1Index + s2Index] = s2[s2Index++];
+            } else if (s1[s1Index] < s2[s2Index]) {
+                result[s1Index + s2Index] = s1[s1Index++];
+            } else {
+                result[s1Index + s2Index] = s2[s2Index++];
+            }
+        }
+
+        return result;
+    }
 
     private static int[] sort(int[] nums) {
-        return nums;
+        return sort(nums, 0, nums.length);
     }
 
     @Test
