da2736fa7e6219ace7f534c5a8c86f43b44d6a3f
victormorozov1 <vismo@bk.ru>
2024-06-18 17:30:19 +0300

    Task 3

--- src/test/java/edu/phystech/hw1/AverageTest.java
+++ src/test/java/edu/phystech/hw1/AverageTest.java
@@ -9,19 +9,17 @@ public class AverageTest {
     // количество параметров, то есть вызов функции будет выглядеть так:
     // average(1), average(1, 2), average(1, 2, 3, 4, 5, 10) и тп.
 
-    private static double average(int first) {
-        return first;
+    private static double average(int... args) {
+        if (args.length == 0) {
+            return 0;
+        }
+
+        double sum = 0;
+        for (int arg : args) {
+            sum += arg;
+        }
+        return sum / args.length;
     }
-
-    private static double average(int first, int second) {
-        return (first + second) / 2;
-    }
-
-    private static double average(int first, int second, int third) {
-        return (first + second + third) / 3;
-    }
-
-
 
     @Test
     public void calculateAverage() {
@@ -30,10 +28,8 @@ public class AverageTest {
         Assertions.assertEquals(4d, average(3, 4, 5));
 
 //        Раскомментировать, эти строчки тоже должны работать.
-//        Assertions.assertEquals(0, average());
-//        Assertions.assertEquals(3.5, average(1, 2, 3, 4, 5, 6));
-//        Assertions.assertEquals(6.5, average(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12));
-
-
+        Assertions.assertEquals(0, average());
+        Assertions.assertEquals(3.5, average(1, 2, 3, 4, 5, 6));
+        Assertions.assertEquals(6.5, average(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12));
     }
 }
